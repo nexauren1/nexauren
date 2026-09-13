8d5107df41e7894780c02e40aa2914d21a81fe08
nexauren1 <nexaurenstore@gmail.com>
2026-09-13 20:22:58 +0200

    Add Nexauren G4A Google Analytics loader

---
+++ frontend/js/G4A.js
@@ -0,0 +1,40 @@
+(() => {
+  const GA_ID = "G-Y4K8V974Q1";
+
+  if (!GA_ID || window.__nexaurenG4A) return;
+  window.__nexaurenG4A = true;
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function () {
+    window.dataLayer.push(arguments);
+  };
+
+  window.gtag("consent", "default", {
+    analytics_storage: "denied",
+    ad_storage: "denied",
+    ad_user_data: "denied",
+    ad_personalization: "denied",
+    wait_for_update: 500
+  });
+
+  window.gtag("set", "ads_data_redaction", true);
+
+  const script = document.createElement("script");
+  script.async = true;
+  script.src =
+    `https://www.googletagmanager.com/gtag/js?id=${encodeURIComponent(GA_ID)}`;
+
+  script.onload = () => {
+    window.gtag("js", new Date());
+    window.gtag("config", GA_ID, {
+      send_page_view: true,
+      anonymize_ip: true
+    });
+  };
+
+  script.onerror = () => {
+    window.__nexaurenG4A = false;
+  };
+
+  document.head.appendChild(script);
+})();
